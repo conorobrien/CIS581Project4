7f330932c4048c7db7854753c0f66529ee36577a
Conor O'Brien <conoreobrien@gmail.com>
2014-12-09 01:25:18 -0500

    Added to test_builtin to show what we're trying to do using feature extraction

--- test_builtin.m
+++ test_builtin.m
@@ -6,25 +6,47 @@ lEyeDetector = vision.CascadeObjectDetector('lefteye');
 rEyeDetector = vision.CascadeObjectDetector('righteye');
 mouthDetector = vision.CascadeObjectDetector('mouth');
 noseDetector = vision.CascadeObjectDetector('nose');
+
 images = load_images('SampleSet/hard');
 
 for i = 1:numel(images)
     I = images{i};
     
-    fprintf('working on image %d/%d \n', i, numel(images));
-    clf; imagesc(I); axis image; axis off; drawnow;
+    fprintf('Detecting faces in image %d/%d ... \n', i, numel(images));
+    figure(1);clf;subplot(1,2,1);
+    subimage(I); axis image; axis off; drawnow;hold on
     faceboxes = step(faceDetector, I);
-    rEyeBoxes = step(rEyeDetector, I);
-    lEyeBoxes = step(lEyeDetector, I);
-    mouthBoxes = step(mouthDetector, I);
-    noseBoxes = step(noseDetector, I);
     
-    IFaces = insertObjectAnnotation(I, 'rectangle', faceboxes, 'Face');
-    IFaces = insertObjectAnnotation(IFaces, 'rectangle', rEyeBoxes, 'Right', 'color', 'red');
-    IFaces = insertObjectAnnotation(IFaces, 'rectangle', lEyeBoxes, 'Left', 'color', 'green');
-    IFaces = insertObjectAnnotation(IFaces, 'rectangle', mouthBoxes, 'Mouth','color', 'blue');
-    IFaces = insertObjectAnnotation(IFaces, 'rectangle', noseBoxes, 'Nose', 'color', 'cyan');
+    fprintf('Found %d faces \n', size(faceboxes, 1));
+    
+    for j = 1:size(faceboxes,1)
+        
+        fprintf('Detecting features in face %d/%d ... \n', j, size(faceboxes, 1));
 
-    figure, imshow(IFaces)
+        xrange = (1:faceboxes(j,4)) + faceboxes(j,2);
+        yrange = (1:faceboxes(j,3)) + faceboxes(j,1);
+        faceimage = I(xrange, yrange,:);
+        rEyeBoxes = step(rEyeDetector, faceimage);
+        lEyeBoxes = step(lEyeDetector, faceimage);
+        mouthBoxes = step(mouthDetector, faceimage);
+        noseBoxes = step(noseDetector, faceimage);
+        annotations = [ repmat({'Right Eye'}, size(rEyeBoxes,1),1);
+                        repmat({'Left Eye'}, size(lEyeBoxes,1),1);
+                        repmat({'Mouth'}, size(mouthBoxes,1),1);
+                        repmat({'Nose'}, size(noseBoxes,1),1)];
+
+        subimageAnn = insertObjectAnnotation(faceimage, 'rectangle', [rEyeBoxes; lEyeBoxes; mouthBoxes; noseBoxes], annotations);
+        subplot(1,2,2);
+        subimage(subimageAnn); axis image; axis off; drawnow;
+        
+        subplot(1,2,1)
+        IFaces = insertObjectAnnotation(I, 'rectangle', faceboxes(j,:), 'Face');
+        subimage(IFaces)
+        
+        disp('Paused...')
+        pause
+    end
+
+
     pause
 end
