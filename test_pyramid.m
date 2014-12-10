3cdd7b87d6140a1a8d609765ef0354f6c4204106
Conor O'Brien <conoreobrien@gmail.com>
2014-12-10 02:01:39 -0500

    Revised pyramidBlur to be simpler and faster

--- test_pyramid.m
+++ test_pyramid.m
@@ -1,30 +1,7 @@
 %% builtin function
 
-I0 = imread('cameraman.tif');
-I1 = impyramid(I0, 'reduce');
-I2 = impyramid(I1, 'reduce');
-I3 = impyramid(I2, 'reduce');
-
-figure(1)
-subplot(1,4,1)
-subimage(I0)
-subplot(1,4,2)
-subimage(I1)
-subplot(1,4,3)
-subimage(I2)
-subplot(1,4,4)
-subimage(I3)
-
-%% our pyramid functions, not exactly the same but pretty close
-
-pyramids = pyramidMake(im2double(I0), 4);
-
-figure(2)
-l = length(pyramids);
-subplot(2,l,1);
-for i = 1:length(pyramids)
-    subplot(2,l,i)
-    subimage(pyramids(i).gaussian);
-    subplot(2, l, i+l)
-    subimage(pyramids(i).laplacian);
-end
+im1 = imread('apple.jpg');
+im2 = imread('orange.jpg');
+mask = [ones(size(im1,1), size(im1,2)/2) zeros(size(im1,1), size(im1,2)/2)];
+figure(3)
+imagesc(pyramidBlur(im1, im2, mask)); axis image off
